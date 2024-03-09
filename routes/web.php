bbf53e0efead333eff0ec44d16456c9cc1dd772f
nemetviiktor <dajlefromcb@gmail.com>
2024-03-09 17:39:56 +0100

    Route set up

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AppointmentsController;
 
 Route::resource('/', AppointmentsController::class);
+
+//Route::get('/appointments', [AppointmentsController::class, 'index']);
