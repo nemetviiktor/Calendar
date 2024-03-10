1b408b260653299d88427067f48ec631b7bd9ce7
nemetviiktor <dajlefromcb@gmail.com>
2024-03-10 16:17:52 +0100

    removed use

--- app/Http/Controllers/AppointmentsController.php
+++ app/Http/Controllers/AppointmentsController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Appointment;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\App;
 
 class AppointmentsController extends Controller
 {
