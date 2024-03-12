fe236802d16c5a24b3c1e1798f43f64e9825fae6
nemetviiktor <dajlefromcb@gmail.com>
2024-03-12 13:38:07 +0100

    checkAppointmentConflict first version

--- app/Http/Controllers/AppointmentsController.php
+++ app/Http/Controllers/AppointmentsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Appointment;
 use Illuminate\Http\Request;
 use App\Helpers\OpeningHoursUtility;
+use Carbon\Carbon;
 
 class AppointmentsController extends Controller
 {
@@ -29,24 +30,39 @@ class AppointmentsController extends Controller
                 'title' => $appointment->username
             ];
         }
-
+        //$appointment = $this->getAppointmentByStart()
         return response()->json($data);
+    }
+
+    public function checkAppointmentConflict($start, $end)
+    {
+        $conflictingAppointments = Appointment::where(function ($query) use ($start, $end) {
+            $query->where('start', '<', $end)
+                ->where('end', '>', $start);
+        })->get();
+
+        return $conflictingAppointments->isNotEmpty();
     }
 
     public function store(Request $request)
     {
+        $start = $request->input('start');
+        $end = $request->input('end');
 
+        $conflict = $this->checkAppointmentConflict($start, $end);
         $openingHoursUtility = new OpeningHoursUtility();
 
-        if ($openingHoursUtility->isWithinOpeningHours($request->input('start')))
-        {
+        if ($conflict) {
+            return response()->json(['message' => 'Appointment conflicts with existing event']);
+        }
+
+        if ($openingHoursUtility->isWithinOpeningHours($start)) {
             $appointment = new Appointment();
             $appointment->fill($request->only('start', 'end', 'username'));
             $appointment->save();
-            return response()->json(['message' => 'Event updated successfully']);
-        }
-        else
-        {
+
+            return response()->json(['message' => 'Event created successfully']);
+        } else {
             return response()->json(['message' => 'Not in opening hours']);
         }
     }
