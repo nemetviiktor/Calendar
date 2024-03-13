abe9eeaf81c51bfd49c8130a5ecfec5c05480011
nemetviiktor <dajlefromcb@gmail.com>
2024-03-13 10:56:02 +0100

    moved isWithinOpeningHours function to openinghoursservice

--- app/Helpers/OpeningHoursUtility.php
+++ app/Helpers/OpeningHoursUtility.php
@@ -3,8 +3,7 @@
 namespace App\Helpers;
 
 use App\Services\OpeningHoursService;
-use RRule\RRule;
-use Carbon\Carbon;
+
 
 class OpeningHoursUtility
 {
@@ -29,36 +28,5 @@ class OpeningHoursUtility
         $minutes = $totalMinutes % 60;
 
         return sprintf('%02d:%02d', $hours, $minutes);
-    }
-
-    public function isWithinOpeningHours($date) {
-
-        $openingHoursService = new OpeningHoursService();
-        $openingHoursData = $openingHoursService->getOpeningHoursData();
-
-        $occurrences = [];
-        foreach ($openingHoursData as $openingHourData) {
-            if (isset($openingHourData['rrule'])) {
-                $rule = new RRule([
-                    'FREQ' => $openingHourData['rrule']['freq'],
-                    'INTERVAL' => $openingHourData['rrule']['interval'],
-                    'BYDAY' => $openingHourData['rrule']['byweekday'],
-                    'DTSTART' => $openingHourData['rrule']['dtstart'],
-                    'UNTIL' => '2024-12-31'
-                ]);
-                $occurrences = array_merge($occurrences, $rule->getOccurrences());
-            } else {
-                $occurrences[] = Carbon::parse($openingHourData['start']);
-            }
-        }
-
-        $dateToSearch = Carbon::parse($date)->toDateString();
-        foreach ($occurrences as $occurrence) {
-            $formattedDate = $occurrence->format('Y-m-d');
-            if ($formattedDate === $dateToSearch) {
-                return true;
-            }
-        }
-        return false;
     }
 }
--- app/Services/OpeningHoursService.php
+++ app/Services/OpeningHoursService.php
@@ -4,6 +4,8 @@ namespace App\Services;
 
 use App\Models\OpeningHours;
 use App\Helpers\OpeningHoursUtility;
+use RRule\RRule;
+use Carbon\Carbon;
 
 class OpeningHoursService
 {
@@ -53,5 +55,35 @@ class OpeningHoursService
             'end' => $this->openingHoursUtility->getEnd($openingHour->dtstart, $openingHour->time),
             'groupId' => 'openingHours'
         ];
+    }
+
+    public function isWithinOpeningHours($date) {
+
+        $openingHoursData = $this->getOpeningHoursData();
+
+        $occurrences = [];
+        foreach ($openingHoursData as $openingHourData) {
+            if (isset($openingHourData['rrule'])) {
+                $rule = new RRule([
+                    'FREQ' => $openingHourData['rrule']['freq'],
+                    'INTERVAL' => $openingHourData['rrule']['interval'],
+                    'BYDAY' => $openingHourData['rrule']['byweekday'],
+                    'DTSTART' => $openingHourData['rrule']['dtstart'],
+                    'UNTIL' => '2024-12-31'
+                ]);
+                $occurrences = array_merge($occurrences, $rule->getOccurrences());
+            } else {
+                $occurrences[] = Carbon::parse($openingHourData['start']);
+            }
+        }
+
+        $dateToSearch = Carbon::parse($date)->toDateString();
+        foreach ($occurrences as $occurrence) {
+            $formattedDate = $occurrence->format('Y-m-d');
+            if ($formattedDate === $dateToSearch) {
+                return true;
+            }
+        }
+        return false;
     }
 }
