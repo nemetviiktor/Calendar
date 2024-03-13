e087763a9af042f8e25189d4ebc81b8b17f7463a
nemetviiktor <dajlefromcb@gmail.com>
2024-03-13 10:33:45 +0100

    dependency injecting

--- app/Services/OpeningHoursService.php
+++ app/Services/OpeningHoursService.php
@@ -7,27 +7,29 @@ use App\Helpers\OpeningHoursUtility;
 
 class OpeningHoursService
 {
+    private $openingHoursUtility;
+
+    public function __construct(OpeningHoursUtility $openingHoursUtility)
+    {
+        $this->openingHoursUtility = $openingHoursUtility;
+    }
+
     public function getOpeningHoursData()
     {
         $openingHours = OpeningHours::all();
         $data = [];
 
-        $openingHoursUtility = new OpeningHoursUtility();
-
         foreach ($openingHours as $openingHour) {
-            if ($openingHour->freq !== null)
-            {
-                $data[] = $this->getRecurringOpeningHourData($openingHour, $openingHoursUtility);
-            }
-            else
-            {
-                $data[] = $this->getSingleOpeningHourData($openingHour, $openingHoursUtility);
+            if ($openingHour->freq !== null) {
+                $data[] = $this->getRecurringOpeningHourData($openingHour);
+            } else {
+                $data[] = $this->getSingleOpeningHourData($openingHour);
             }
         }
         return $data;
     }
 
-    private function getRecurringOpeningHourData($openingHour, $openingHoursUtility)
+    private function getRecurringOpeningHourData($openingHour)
     {
         return [
             'display' => 'background',
@@ -35,20 +37,20 @@ class OpeningHoursService
                 'freq' => $openingHour->freq,
                 'interval' => (int)$openingHour->interval,
                 'byweekday' => $openingHour->byweekday,
-                'dtstart' => $openingHoursUtility->getDtStart($openingHour->dtstart, $openingHour->time),
+                'dtstart' => $this->openingHoursUtility->getDtStart($openingHour->dtstart, $openingHour->time),
                 'until' => $openingHour->until
             ],
-            'duration' => $openingHoursUtility->getDuration($openingHour->time),
+            'duration' => $this->openingHoursUtility->getDuration($openingHour->time),
             'groupId' => 'openingHours'
         ];
     }
 
-    private function getSingleOpeningHourData($openingHour, $openingHoursUtility)
+    private function getSingleOpeningHourData($openingHour)
     {
         return [
             'display' => 'background',
-            'start' => $openingHoursUtility->getDtStart($openingHour->dtstart, $openingHour->time),
-            'end' => $openingHoursUtility->getEnd($openingHour->dtstart, $openingHour->time),
+            'start' => $this->openingHoursUtility->getDtStart($openingHour->dtstart, $openingHour->time),
+            'end' => $this->openingHoursUtility->getEnd($openingHour->dtstart, $openingHour->time),
             'groupId' => 'openingHours'
         ];
     }
